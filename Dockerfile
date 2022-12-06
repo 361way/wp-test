10a1af73b281684404ca01578d21119b0840cf66
itybku <itybku@gmail.com>
2022-12-07 02:41:10 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:22.10
+
+RUN  apt update \
+     && ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
+     && apt -y install apache2 php php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip php-opcache php-redis mariadb-client php-mysql sendmail\
+     && apt-get clean \
+     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* 
+	 
+COPY wp src /srv/
+COPY docker-entrypoint.sh /usr/local/bin/
+
+VOLUME /var/www/html/
+EXPOSE 80
+
+ENTRYPOINT ["/bin/bash","/usr/local/bin/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+export  PATH=/usr/local/bin/:$PATH
+
+if [ -d /srv/wordpress ];then
+   rm -rf /var/www/html/index.html
+   mv /srv/wordpress/* /var/www/html/
+   mv /srv/wordpress/.htaccess /var/www/html/
+   mv /srv/wp /usr/local/bin/
+   chmod +x /usr/local/bin/wp
+   rm -rf /srv/wordpress/
+   mv /srv/env /root/
+   source /root/env
+fi
+
+
+
+if [ ! -s /var/www/html/wp-config.php ];then
+	wp core config --dbname=$STAGE_WORDPRESS_DB_NAME --dbuser=$STAGE_WORDPRESS_DB_USER --dbpass=$STAGE_WORDPRESS_DB_PASSWORD --dbhost=$STAGE_WORDPRESS_DB_HOST --allow-root --path=/var/www/html/
+
+	#wp core install --url="http://127.0.0.1"  --title="Blog Title" --admin_user="admin" --admin_password="password" --admin_email="itybku@139.com"  --allow-root --path=/var/www/html/
+	wp core install --url=$URL  --title=$Title --admin_user=$Admin --admin_password=$PASSWORD --admin_email=$Email  --allow-root --path=/var/www/html/
+
+	#wp config set WP_REDIS_HOST "127.0.0.1" --allow-root --path=/var/www/html/
+	#wp config set WP_REDIS_PORT 6379 --allow-root --path=/var/www/html/
+	#wp config set WP_REDIS_PASSWORD password --allow-root --path=/var/www/html/
+
+	if [ $REDIS_HOST ];then
+
+		wp config set WP_REDIS_HOST $REDIS_HOST --allow-root --path=/var/www/html/
+		
+		if [ $REDIS_PORT ];then
+			wp config set WP_REDIS_PORT $REDIS_PORT --allow-root --path=/var/www/html/
+		else
+			echo "Use the Redis define port 6379"
+			wp config set WP_REDIS_PORT 6379 --allow-root --path=/var/www/html/
+		fi
+		
+		if [ $REDIS_PASSWORD ];then
+			wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root --path=/var/www/html/
+		else
+			echo "Redis cache use the empty password"
+		fi
+		
+		wp plugin install redis-cache --activate
+		
+	else
+		echo "REDIS_HOST parameter didn't set, didn't need configure the Redis cache"
+	fi
+
+fi
+
+chown -R www-data:www-data /var/www/html/*
+/etc/init.d/apache2 start
+tail -f /var/log/apache2/access.log
